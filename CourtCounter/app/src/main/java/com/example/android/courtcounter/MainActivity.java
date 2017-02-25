592a7b3b527cba69259a1cdaa0c0e03067838465
ezbery <brenin0204@gmail.com>
2017-02-25 19:08:55 +0100

    on click methods for buttons

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(0);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamA(View v) {
+        displayForTeamA(3);
+    }
+
+    public void addTwoForTeamA(View v) {
+        displayForTeamA(2);
+    }
+
+    public void addOneForTeamA(View v) {
+        displayForTeamA(1);
+    }
+
 }
