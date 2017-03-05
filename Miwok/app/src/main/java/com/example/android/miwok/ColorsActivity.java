c55adb9dca94a1f058f10719021299527a16391f
ezbery <brenin0204@gmail.com>
2017-03-05 21:59:41 +0100

    passing color to WordAdapter and showing in layouts

--- Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -23,7 +23,7 @@ public class ColorsActivity extends AppCompatActivity {
         words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray));
         words.add(new Word("black", "kululli", R.drawable.color_black));
         words.add(new Word("white", "kelelli", R.drawable.color_white));
-        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_colors);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
     }
