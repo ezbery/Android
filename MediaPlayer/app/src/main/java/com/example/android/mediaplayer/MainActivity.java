b803579a5aac21dbc9584beea0d6c9c8fe38530e
ezbery <brenin0204@gmail.com>
2017-03-11 11:23:55 +0100

    audio play/pause buttons working

--- MediaPlayer/app/src/main/java/com/example/android/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/android/mediaplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.mediaplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer audio1MediaPlayer = MediaPlayer.create(this, R.raw.audio_1);
+
+        // Find the View
+        Button playButton = (Button) findViewById(R.id.playButton);
+        Button pauseButton = (Button) findViewById(R.id.pauseButton);
+
+        // Set a click listeners
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                //startActivity(colorsIntent);
+                audio1MediaPlayer.start();
+            }
+        });
+
+        // Set a click listeners
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                //startActivity(colorsIntent);
+                audio1MediaPlayer.pause();
+            }
+        });
     }
 }
